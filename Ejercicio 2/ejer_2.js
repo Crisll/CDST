b679e76ec2904a54ae3890df0307e10684ced62e
Crisll <chrisdasilvatan@gmail.com>
2023-07-17 17:33:13 +0100

    Ejercicio 2 Pseudocódigo

---
+++ Ejercicio 2/ejer_2.js
@@ -0,0 +1,33 @@
+// InicioProceso
+// Escribir &quot;Introduce el primer número&quot;
+// Leer numero1
+// Escribir &quot;Introduce el segundo número&quot;
+// Leer numero2
+// //inicializamos la variable resultado a 0 (recomendable)
+// resultado&lt;-0
+// //sumamos los números y escribimos su resultado
+// resultado&lt;-numero1+numero2
+// Escribir resultado
+// //restamos los números y escribimos su resultado
+// resultado&lt;-numero1-numero2
+// Escribir resultado
+// //multiplicamos los números y escribimos su resultado
+// resultado&lt;-numero1*numero2
+// Escribir resultado
+// //dividimos los números y escribimos su resultado
+// resultado&lt;-numero1/numero2
+// Escribir resultado
+var a = Number(prompt("Introduce el primer número"));
+console.log(a);
+var b = Number(prompt("Introduce el segundo número"));
+console.log(b);
+var resultado = 0;
+resultado = a + b;
+console.log(resultado);
+resultado = a - b;
+console.log(resultado);
+resultado = a * b;
+console.log(resultado);
+resultado = a / b;
+console.log(resultado);
+
