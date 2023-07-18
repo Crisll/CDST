d9f13f20214782f4d1bcb2dfe7fd108a07cc6d04
Crisll <chrisdasilvatan@gmail.com>
2023-07-18 18:16:16 +0100

    Mejora en el código

--- Ejercicio 5 JS/ejer_5.js
+++ Ejercicio 5 JS/ejer_5.js
@@ -2,13 +2,19 @@ var importe = parseInt(prompt("Indique el importe de la compra"));
 console.log(importe);
 var mes = prompt("Indique el mes actual");
 console.log(mes);
-
-if (mes=="octubre") {
+// Se puede usar mes.toLowerCase para que todo lo que introduzca el usuario a miniscula y evitar todo el tinglado.
+if (mes.toLowerCase()=="octubre") {
     console.log(importe*0.85)
 }
-else if (mes=="Octubre") {
-    console.log(importe*0.85)
-}
+// else if (mes=="Octubre") {
+//     console.log(importe*0.85)
+// }
+// else if (mes=="OCTUBRE") {
+//     console.log(importe*0.85)
+// }
+// else if (mes=="oCTUBRE") {
+//     console.log(importe*0.85)
+// }
 else {
     console.log(importe)
 }
