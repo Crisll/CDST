60338ed2dab054a0dc5151421cb34ca15b50f8d3
Crisll <chrisdasilvatan@gmail.com>
2023-07-18 16:00:21 +0100

    Ejercicio 3 de Pseudocódigo

---
+++ Ejercicio 3 JS/ejer_3.js
@@ -0,0 +1,36 @@
+// InicioProceso
+// Escribir &quot;Introduce el primer número&quot;
+// Leer numero1
+// Escribir &quot;Introduce el segundo número&quot;
+// Leer numero2
+// //comparamos los dos números,
+// //si el primero es mayor o igual que el segundo entra
+// Si (numero1&gt;=numero2) Entonces
+// //Si el numero1 y numero2 son iguales entra y escribe que son iguales
+// //Sino lo son escribe que el numero1 es el mayor
+// Si (numero1=numero2) Entonces
+// escribir &quot;los números &quot; numero1 &quot; &quot; numero2 &quot; son iguales&quot;
+// Sino
+// Escribir numero1 &quot; es el mayor de los dos&quot;
+// FinSi
+// //Si el primer Si es falso, escribe que el numero2 es mayor
+// Sino
+// Escribir numero2 &quot; es el mayor de los dos&quot;
+// FinSi
+// FinProceso
+
+var a = parseInt(prompt("Introduce el primer número"));
+console.log(a);
+var b = parseInt(prompt("Introduce el segundo número"));
+console.log(b);
+
+if (a>b) { 
+    console.log("A es mayor que B");
+}
+else  if (a==b) {
+    console.log("A es igual que B");
+}
+else {
+    console.log("A es menor que B");
+}
+
