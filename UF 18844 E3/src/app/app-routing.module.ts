295f981c74b857f8c9489da9438196a96b2a948b
Crisll <chrisdasilvatan@gmail.com>
2023-08-31 18:37:00 +0100

    UF 1844 E3

---
+++ UF 18844 E3/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './contacto/contacto.component';
+import { EjecucionComponent } from './ejecucion/ejecucion.component';
+import { LoginComponent } from './login/login.component';
+import { PresupuestoComponent } from './presupuesto/presupuesto.component';
+import { RegistroComponent } from './registro/registro.component';
+import { MetodoComponent } from './metodo/metodo.component';
+
+const routes: Routes = [
+  {path: "Contacto", component: ContactoComponent},
+  {path: "Ejecucion", component: EjecucionComponent},
+  {path: "Login", component: LoginComponent},
+  {path: "Presupuesto", component: PresupuestoComponent},
+  {path: "Registro", component: RegistroComponent},
+  {path: "Metodo", component: MetodoComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
